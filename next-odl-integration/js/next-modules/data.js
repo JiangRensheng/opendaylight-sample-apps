cc83a7c64dccc36a7ae42c847736171b80405a7d
Jiang Rensheng <13841495@qq.com>
2016-06-12 10:43:01 +0800

    check whether topology's node and link is not defined to topology display nothing for runtime error

--- next-odl-integration/js/next-modules/data.js
+++ next-odl-integration/js/next-modules/data.js
@@ -7,25 +7,33 @@ var odl2next = function (data) {
 	// prepare stub for results
 	var topologyResult = {nodes: [], links: []};
 	// process nodes
-	for (var i = 0; i < topology.node.length; i++) {
-		var node = {};
-		// node name
-		if (topology.node[i].hasOwnProperty('node-id')) {
-			node.name = topology.node[i]['node-id'];
+	if (typeof(topology.node) == "undefined") { 
+	} else {
+		for (var i = 0; i < topology.node.length; i++) {
+			var node = {};
+			// node name
+			if (topology.node[i].hasOwnProperty('node-id')) {
+				node.name = topology.node[i]['node-id'];
+			}
+			// ... other actions with node object ...
+			// add the node to the result object
+			topologyResult.nodes.push(node);
 		}
-		// ... other actions with node object ...
-		// add the node to the result object
-		topologyResult.nodes.push(node);
 	}
 	// processing links
-	for (i = 0; i < topology.link.length; i++) {
-		var link = {
-			id: i,
-			source: topology.link[i].source['source-node'],
-			target: topology.link[i].destination['dest-node']
-		};
-		// add the link to the result object
-		topologyResult.links.push(link);
+	if (typeof(topology.link) == "undefined") { 
+	} 
+	else 
+	{
+		for (i = 0; i < topology.link.length; i++) {
+			var link = {
+				id: i,
+				source: topology.link[i].source['source-node'],
+				target: topology.link[i].destination['dest-node']
+			};
+			// add the link to the result object
+			topologyResult.links.push(link);
+		}
 	}
 	return topologyResult;
 };
